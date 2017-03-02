e4a5ab880708a0d2ea5d28e86413c11cee91023c
mehdi254 <mehdi.sebih@gmail.com>
2017-03-02 23:54:03 +0100

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -26,25 +26,34 @@ public class Parser {
 	
 	public static void main(String[] args) throws Exception {
 		
-		File file = new File("/home/smehdi254/Desktop/Kraya/TER/Articles/S/Syndrome 3C.txt");
+		File file = new File("/home/smehdi254/Desktop/Kraya/TER/Articles/R/Radiographie du thorax.txt");
 		FileInputStream fis = new FileInputStream(file);
 		byte[] data = new byte[(int) file.length()];
 		fis.read(data);
 		fis.close();
 		String str = new String(data, "UTF-8");
 		
-		Parser c = new Parser();
+		Chargement c = new Chargement();
 		
-		Pattern pattern = Pattern.compile("\\{\\{((.)+?)\\}\\}");
-
-	        Matcher matcher = pattern.matcher(str);
+		Pattern pattern = Pattern.compile("\\{\\{Lang\\|\\w\\w\\|((.)+?)\\}\\}");
+        	Matcher matcher = pattern.matcher(str);
+        	while (matcher.find()) {
+	        	String group = matcher.group(0);
+	        	String group1 = matcher.group(1);
+	        	str=str.replace(group, group1);
+	        	group=group.replace(group1, "");
+	            c.deleted.add(group);
+	        }
+        
+	        pattern = Pattern.compile("\\{\\{\\w\\w\\}\\}");
+	        matcher = pattern.matcher(str);
 	        while (matcher.find()) {
 	            String group = matcher.group(0);
-            		c.deleted.add(group);
-	        }
-	        str=matcher.replaceAll("");
+	            c.deleted.add(group);
+        	}
+        	str=matcher.replaceAll("");
 	        
-        	pattern = Pattern.compile("\\{\\{((.|\n)+?)\\}\\}|\\[\\[Catégorie:((.)+?)\\]\\]|\\[http((.)+?)\\]");
+	        pattern = Pattern.compile("\\{\\{(.)+?\\}\\}");
 	        matcher = pattern.matcher(str);
 	        while (matcher.find()) {
 	            String group = matcher.group(0);
@@ -52,43 +61,74 @@ public class Parser {
 	        }
 	        str=matcher.replaceAll("");
 	        
-	        pattern = Pattern.compile("\\[\\[((.)+?)\\]\\]");
+        	pattern = Pattern.compile("<ref([^>])+?\\/>");
 	        matcher = pattern.matcher(str);
-        	while (matcher.find()) {
-	            String group = matcher.group(1);
-	            c.linksWiki.add(group);
+	        while (matcher.find()) {
+	            String group = matcher.group(0);
+	            c.deleted.add(group);
 	        }
-	        str=str.replaceAll("\\[\\[","");
-	        str=str.replaceAll("\\]\\]","");
+	        str=matcher.replaceAll("");
+	        
         
-        	pattern = Pattern.compile("''((.)+)''");
+	        pattern = Pattern.compile("(<ref(.|\n)*?\\/ref>)+?|\\[\\[Catégorie:((.)+?)\\]\\]|\\[\\[Fichier:((.)+?)\\]\\]\n|\\[http((.)+?)\\]|<!--(.)?-->");
         	matcher = pattern.matcher(str);
 	        while (matcher.find()) {
-	            String group = matcher.group(1);
+	            String group = matcher.group(0);
+	            c.deleted.add(group);
+	        }
+	        str=matcher.replaceAll("");
+        
+	        pattern = Pattern.compile("\\[\\[([^|]+?)\\]\\]");
+	        matcher = pattern.matcher(str);
+	        while (matcher.find()) {
+	        	String group = matcher.group(0);
+	        	String group1 = matcher.group(1);
+	        	str=str.replace(group, group1);
+	            	c.linksWiki.add(group1);            
+        	}
+        
+        	pattern = Pattern.compile("\\[\\[((.)+?\\|((.)+?))\\]\\]");
+        	matcher = pattern.matcher(str);
+        	while (matcher.find()) {
+	        	String group = matcher.group(0);
+        		String group1 = matcher.group(3);
+	        	str=str.replace(group, group1);
+	        	group=group.replace(group1, "");
+	                c.deleted.add(group);
+	        	c.linksWiki.add(group1);
+        	}
+        
+        
+        	str=str.replaceAll("<br(\\s)*?/>","\n");
+        
+	        pattern = Pattern.compile("''((.)+)''");
+	        matcher = pattern.matcher(str);
+        	while (matcher.find()) {
+	            	String group = matcher.group(1);
 	            if (group.charAt(0)!='\'') {if (group.length()>1) c.italic.add(group);}
 	            else if (group.charAt(1)!='\'') {group = group.substring(1, group.length()-1); if (group.length()>1) c.bold.add(group);}
 	            else {group = group.substring(3,group.length()-3); if (group.length()>1) c.boldItalic.add(group);}
 	        }
-        	str=str.replaceAll("('){2,}","");
+	        str=str.replaceAll("('){2,}","");
         
 	        pattern = Pattern.compile("==((.)+)==");
 	        matcher = pattern.matcher(str);
-        	while (matcher.find()) {
+	        while (matcher.find()) {
 	        	String group = matcher.group(1).trim();
 	            c.sectionTitles.add(group);
 	        }
-	        
+        
         	pattern = Pattern.compile("((.|\n)+?)\n==");
-	        matcher = pattern.matcher(str);
+        	matcher = pattern.matcher(str);
         	while (matcher.find()) {
-	        	String group = matcher.group(1).trim();
-	            c.sectionTexts.add(group);
+        		String group = matcher.group(1).trim();
+		        c.sectionTexts.add(group);
 	        }
-	        
-	        str=str.replaceAll("(\\h){2,}"," ");
-	        str=str.replaceAll("(\n){3,}","\n\n");
-	        str=str.trim();
-
+        
+        	str=str.replaceAll("(\\h){2,}"," ");
+        	str=str.replaceAll("(\n){3,}","\n\n");
+        	str=str.trim();
+		
 	}
 
 }
