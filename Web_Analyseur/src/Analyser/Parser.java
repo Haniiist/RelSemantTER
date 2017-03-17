8fc86149fd1717c4df8e4a139495d64fc85d7c5f
smehdi254 <mehdi.sebih@gmail.com>
2017-03-17 15:18:44 +0100

    Réorganisation

--- Web_Analyseur/src/Analyser/Parser.java
+++ Web_Analyseur/src/Analyser/Parser.java
@@ -1,13 +1,6 @@
-
-
 import java.io.*;
-import java.net.MalformedURLException;
 import java.util.*;
 import java.util.regex.*;
-
-import RequeterRezo.RequeterRezo;
-import RequeterRezo.Mot;
-import RequeterRezo.RequeterRezo.TupleRelationTerme;
 
 public class Parser {
 	String text;
@@ -18,7 +11,6 @@ public class Parser {
 	ArrayList<String> deleted;
 	ArrayList<String> sectionTitles;
 	ArrayList<String> sectionTexts;
-	ArrayList<Mot>    motsCompose;
 	
 	public Parser(){
 		text = new String();
@@ -29,34 +21,12 @@ public class Parser {
 		deleted = new ArrayList<String>();
 		sectionTitles = new ArrayList<String>();
 		sectionTexts = new ArrayList<String>();
-		motsCompose = new ArrayList<Mot>();
-	}
-	
-public static Mot RequterRezo(String s){
-		
-		RequeterRezo r = new RequeterRezo();
-		Mot m = null;
-		try {
-			m = r.requete(s);
-			//System.out.println(m);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return m;
-		
 	}
 
 	
 	public static void main(String[] args) throws Exception {
 		
-		File file = new File("D:/TER/Articles/Articles/R/Radiomucite.txt");
+		File file = new File("/home/smehdi254/Desktop/Kraya/TER/Articles/R/Radiographie du thorax.txt");
 		FileInputStream fis = new FileInputStream(file);
 		byte[] data = new byte[(int) file.length()];
 		fis.read(data);
@@ -157,64 +127,8 @@ public static Mot RequterRezo(String s){
         
         	str=str.replaceAll("(\\h){2,}"," ");
         	str=str.replaceAll("(\n){3,}","\n\n");
-        	str=str.replace("."," .");
-        	str=str.replace(","," ,");
-
         	str=str.trim();
-        	String s1 = "" ;
-	        s1= s1+(str);
-	        String[] s = s1.split("\\s");
-			int i =0;
-			//System.out.println("---------------------"+s1+"--------------------------");
-
-
-			int decalage = 6;
-				while(s.length>i){
-					String chaine_mots_compose = "";
-					int fenetre =i;
-					int k=0;
-					while(k<decalage && !s[k].equals("") && k+fenetre<s.length){
-						
-						if(chaine_mots_compose.equals("")) 
-							chaine_mots_compose = s[k+fenetre];
-						else 
-							chaine_mots_compose= chaine_mots_compose+" "+s[k+fenetre];
-						k++;	
-					}
-					System.out.println("---------------------"+chaine_mots_compose+"--------------------------");
-					int bool=0;
-					if (!Arrays.asList(new String[]{" ",".  ",". ",".","La","de la",".","Elle"}).contains(chaine_mots_compose)) {
-						Mot compound_word = RequterRezo(chaine_mots_compose);
-
-						if(compound_word !=null){
-							bool=1;
-							c.motsCompose.add(compound_word );
-							String compound_word_underscore = compound_word.getNom().toString();
-							System.out.println("**********"+compound_word_underscore+"*************");
-
-							compound_word_underscore=compound_word_underscore.replaceAll("\\s", "_");
-							System.out.println("**********"+compound_word_underscore+"*************");
-							str = str.replace(chaine_mots_compose, compound_word_underscore);
-						}
-	                    
-					}	
-					if(bool ==1 || decalage==2){
-                    	i++;
-                    	decalage=6;
-                    }
-                    else 
-                    	decalage--;
-
-				}
-				String s2 = ""+s1;
-				ArrayList<String> copyMots = new ArrayList<String>();
-				for (int a=0;a<c.motsCompose.size();a++){
-					String chaine  =c.motsCompose.get(a).getNom().toString();
-					chaine=chaine.replaceAll("\\s", "_");
-					System.out.println("**********"+chaine+"*************");
-				}
-				
-				//System.out.println("---------------------"+str+"--------------------------");
+		
 	}
 
 }
