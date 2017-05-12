1fc5245b720ff47320b80171340b071cf45130a0
smehdi254 <mehdi.sebih@gmail.com>
2017-05-12 17:28:16 +0200

    Modification Lemmatisation et Parser

--- RelSem/src/Analyseur/Lemmatisation.java
+++ RelSem/src/Analyseur/Lemmatisation.java
@@ -11,17 +11,17 @@ import java.util.Scanner;
 
 
 public class Lemmatisation extends TextClass{
-	
+
 	//String oldText;
 	//String newText;
 	static HashMap<String, ArrayList<String>> map;
-	
+
 	public Lemmatisation () throws IOException {
 		createDico("dico.txt");
 		oldText=new String();
 		newText=new String();
 	}
-	
+
 	public Lemmatisation (TextClass tc) {
 		oldText=tc.newText;
 		try {
@@ -32,111 +32,134 @@ public class Lemmatisation extends TextClass{
 		}
 		newText=lemmatizeText();
 	}
-	
+
 	public static void createDico (String filePath) throws IOException {
-	    map = new HashMap<String, ArrayList<String>>();
-	    String line;
-	    BufferedReader reader = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8);
-	    while ((line = reader.readLine()) != null)
-	    {
-	        String[] parts = line.split("	");
-	        if (parts.length >= 2)
-	        {
-	            String key = parts[0];
-	            String value = parts[1];
-	            
-	            String deux = parts[1]+"	"+parts[2];
-	            
-	            if (map.keySet().contains(parts[0])) {
-	            	map.get(parts[0]).add(deux);
-	            }else {
-	            	ArrayList<String> values = new ArrayList<String>();
-	            	values.add(deux);
-	            	map.put(key, values);
-	            }
-	            	
-	        } else {
-	            System.out.println(line);
-	        }
-	    }
+		map = new HashMap<String, ArrayList<String>>();
+		String line;
+		BufferedReader reader = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8);
+		while ((line = reader.readLine()) != null)
+		{
+			String[] parts = line.split("	");
+			if (parts.length >= 2)
+			{
+				String key = parts[0];
+				String value = parts[1];
+
+				String deux = parts[1]+"	"+parts[2];
+
+				if (map.keySet().contains(parts[0])) {
+					map.get(parts[0]).add(deux);
+				}else {
+					ArrayList<String> values = new ArrayList<String>();
+					values.add(deux);
+					map.put(key, values);
+				}
+
+			} else {
+				System.out.println(line);
+			}
+		}
 	}
-	
+
 	/*public String lemmatizeVerbsOnly() {
-		
+
 	}*/
-	
+
 	public String lemmatizeText() {
 		String str = new String(oldText);
-        String[] s = str.split("\\s");
-        String res = new String();
-        for (int i=0; i<s.length;i++) {
+		String[] s = str.split("\\s");
+		String res = new String();
+		for (int i=0; i<s.length;i++) {
 			if (howManyLemmes(s[i])==1 && getPos(s[i]).equals("Ver")) {
-                if(!s[i].toLowerCase().equals("est") && !s[i].toLowerCase().equals("sont") && !s[i].toLowerCase().equals("a")){
-    				//System.out.println("  s[i] "+(s[i]));
+				if(!s[i].toLowerCase().equals("est") && !s[i].toLowerCase().equals("sont") && !s[i].toLowerCase().equals("a")){
+					//System.out.println("  s[i] "+(s[i]));
 
-    				//System.out.println("  Lemma "+getLemme(s[i]));
-                	s[i]=getLemme(s[i]);
-                }
+					//System.out.println("  Lemma "+getLemme(s[i]));
+					s[i]=getLemme(s[i]);
+				}
 			}
 			res=res+s[i]+" ";
 		}
-        return res;
+		return res;
 	}
-	
+
+	public String lemmatizeText(String str) {
+		String[] s = str.split("\\s|[,.?!:;\\(\\)]+");
+		String res = new String();
+		for (int i=0; i<s.length;i++) {
+			if(!s[i].toLowerCase().equals("est") && !s[i].toLowerCase().equals("sont") && !s[i].toLowerCase().equals("a"))
+				res=res+lemmatize(s[i])+" ";
+			else
+				res=res+s[i]+" ";
+		}
+		return res;
+	}
+
 	public int howManyLemmes (String mot) {
 		int count=0;
 		if (map.keySet().contains(mot))
 			for (String key : map.keySet()){
-			  if(key.equals(mot))
-				  for (String str : map.get(mot)){
+				if(key.equals(mot))
+					for (String str : map.get(mot)){
 						count++;
 					}
-			  }
+			}
 		return count;
 	}
-	
-	
+
+
 
 	public String  getLemme (String mot) {
 		return ((map.get(mot).get(0)).split("\\s"))[0];
 	}
-	
-	public void  getLemmes(String mot) {
-		if (!map.keySet().contains(mot)) System.out.println("Le mot n'existe pas");
-		else {
-			  for (String key : map.keySet()){
-				  if(key.equals(mot))
-					  for (String str : map.get(mot)){
-							String[] tab = str.split("	");
-							//return tab[0];
-							System.out.println("La source du mot "+mot+" est "+tab[0]);
-						}
-				  }
+
+	public String  lemmatize (String mot) {
+		ArrayList<String> tab = map.get(mot);
+		if (tab!=null){ 
+			if (tab.size()==1){
+				if (tab.get(0).split("	")[1].substring(0, 3).equals("Ver"));
+				return tab.get(0).split("\\s")[0];
+			}
 		}
+		return mot;
 	}
-	
-	
+
+	/*public void  getLemmes(String mot) {
+		if (!map.keySet().contains(mot)) String res = mot;ystem.out.println("Le mot n'existe pas");
+		else {
+			for (String key : map.keySet()){
+				if(key.equals(mot))
+					for (String str : map.get(mot)){
+						String[] tab = str.split("	");
+						//return tab[0];
+						System.out.println("La source du mot "+mot+" est "+tab[0]);
+					}
+			}
+		}
+	}*/
+
+
 	//isPluriel isMasculin isFeminin isNom isVerb isAdj isAmbigu (plus d'une correspondance)
-		//getLemme("bois") ==> boire
-		//getPos("bois") ==> verb ou nom ou adj pro det 
-		//getLemme("tapis","Nom") ==> tapis         getLemme("tapis","Ver") ==> tapir
-		// penser Ã  dÃ©clencher des exceptions dans les cas ambigus 
-		// pour spliter sur plusieurs caractÃ¨res split(":|+")   "Nom:Mas+SG"  ==>   [0]="Nom" [1]="Mas" [2]="SG"
-	
+	//getLemme("bois") ==> boire
+	//getPos("bois") ==> verb ou nom ou adj pro det 
+	//getLemme("tapis","Nom") ==> tapis         getLemme("tapis","Ver") ==> tapir
+	// penser Ã  dÃ©clencher des exceptions dans les cas ambigus 
+	// pour spliter sur plusieurs caractÃ¨res split(":|+")   "Nom:Mas+SG"  ==>   [0]="Nom" [1]="Mas" [2]="SG"
+
 	public String  detecter(String mot,String s) throws Exception{
 		if (!map.keySet().contains(mot)) throw new Exception ("Le mot n'existe pas");
 		else {
-			  for (String key : map.keySet()){
-				  if(key.equals(mot))
-					  for (String str : map.get(mot)){
-							String[] tab = str.split("	");
-							if (tab[1].contains(s)) return tab[0];
-						}
-				  }
+			for (String key : map.keySet()){
+				if(key.equals(mot))
+					for (String str : map.get(mot)){
+						String[] tab = str.split("	");
+						if (tab[1].contains(s)) return tab[0];
+					}
+			}
 		}
 		return null;
 	}
-	
+
 	public String getPos(String mot) {
 		if (!map.keySet().contains(mot)) System.out.println("Le mot n'existe pas");
 		else {
@@ -145,7 +168,7 @@ public class Lemmatisation extends TextClass{
 				return tab[1].substring(0, 3);
 			}
 		}
-        return null;
+		return null;
 	}
 
 	public boolean isPluriel(String mot) throws Exception{
@@ -156,9 +179,9 @@ public class Lemmatisation extends TextClass{
 				if (tab[1].contains("PL")) return true;
 			}
 		}
-        return false;
+		return false;
 	}
-	
+
 	public boolean isAdj(String mot) throws Exception{
 		if (!map.keySet().contains(mot)) throw new Exception ("Le mot n'existe pas");
 		else {
@@ -167,10 +190,10 @@ public class Lemmatisation extends TextClass{
 				if (tab[1].contains("Adj")) return true;
 			}
 		}
-        return false;
+		return false;
 	}
-	
-	
+
+
 	public boolean isVerb(String mot) throws Exception{
 		if (!map.keySet().contains(mot)) System.out.println("Le mot n'existe pas");
 		else {
@@ -179,9 +202,9 @@ public class Lemmatisation extends TextClass{
 				if (tab[1].contains("Ver")) return true;
 			}
 		}
-        return false;
+		return false;
 	}
-	
+
 	public boolean isMasculin(String mot) throws Exception{
 		if (!map.keySet().contains(mot)) throw new Exception ("Le mot n'existe pas");
 		else {
@@ -190,10 +213,10 @@ public class Lemmatisation extends TextClass{
 				if (tab[1].contains("Mas")) return true;
 			}
 		}
-        return false;
+		return false;
 	}
-	
-	
+
+
 	public boolean isNom(String mot) throws Exception{
 		if (!map.keySet().contains(mot)) throw new Exception ("Le mot n'existe pas");
 		else {
@@ -202,10 +225,10 @@ public class Lemmatisation extends TextClass{
 				if (tab[1].contains("Nom")) return true;
 			}
 		}
-        return false;
+		return false;
 	}
-	
-	
+
+
 	public boolean isFeminin(String mot) throws Exception{
 		if (!map.keySet().contains(mot)) throw new Exception ("Le mot n'existe pas");
 		else {
@@ -214,30 +237,23 @@ public class Lemmatisation extends TextClass{
 				if (tab[1].contains("Fem")) return true;
 			}
 		}
-        return false;
+		return false;
 	}
-	
-	/*public static void main(String[] args) throws Exception{
-	    
-	    Lemmatisation lm = new Lemmatisation();
-	    
-	    
-	    
-	    System.out.print("Veuillez saisir  : \n");    
+
+	public static void main(String[] args) throws Exception{
+
+		Lemmatisation lm = new Lemmatisation();
+
+		File file = new File("Text.txt");
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+		String str = new String(data, "UTF-8");
+		str=str.replaceAll("'","' ");
+		System.out.println(lm.lemmatizeText(str));
 
 
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	String s = br.readLine();
-
-    	System.out.println(lm.isPluriel(s));
-    	System.out.println(lm.isMasculin(s));
-    	System.out.println(lm.isNom(s));
-    	System.out.println(lm.isFeminin(s));
-    	System.out.println(lm.isVerb(s));
-    	System.out.println(lm.isAdj(s)); 
-    	lm.getLemme(s); 
-    	System.out.println(lm.getPos(s));
-    	System.out.println(lm.detecter(s,"Adj"));
-	}*/
+	}
 
 }
--- RelSem/src/Analyseur/Parser.java
+++ RelSem/src/Analyseur/Parser.java
@@ -129,9 +129,20 @@ public class Parser extends TextClass {
         matcher = pattern.matcher(str);
     	while (matcher.find()) {
             	String group = matcher.group(1);
-            if (group.charAt(0)!='\'') {if (group.length()>1) italic.add(group);}
-            else if (group.charAt(1)!='\'') {group = group.substring(1, group.length()-1); if (group.length()>1) bold.add(group);}
-            else {group = group.substring(3,group.length()-3); if (group.length()>1) boldItalic.add(group);}
+            if (group.charAt(0)!='\'') {
+            	if (group.length()>1) 
+            		italic.add(group);
+            	}
+            else if (group.charAt(1)!='\'') {
+            	group = group.substring(1, group.length()-1); 
+            	if (group.length()>1) 
+            		bold.add(group);
+            	}
+            else {
+            	group = group.substring(3,group.length()-3); 
+            	if (group.length()>1) 
+            		boldItalic.add(group);
+            	}
         }
         str=str.replaceAll("('){2,}","");
     
@@ -171,117 +182,167 @@ public class Parser extends TextClass {
 		
 	}
 		
+	public String cleanText(String str){
+		Pattern pattern = Pattern.compile("\\{\\{Lang\\|\\w\\w\\|((.)+?)\\}\\}");
+		Matcher matcher = pattern.matcher(str);
+    	while (matcher.find()) {
+        	String group = matcher.group(0);
+        	String group1 = matcher.group(1);
+        	str=str.replace(group, group1);
+        	group=group.replace(group1, "");
+            deleted.add(group);
+        }
+    	
+        pattern = Pattern.compile("\\{\\{\\w\\w\\}\\}");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            String group = matcher.group(0);
+            deleted.add(group);
+    	}
+    	str=matcher.replaceAll("");
+    	
+    	pattern = Pattern.compile("\\{\\{([0-9]+e)\\|((.)+?)}\\}");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+        	String group = matcher.group(0);
+        	String group1 = matcher.group(1);
+        	String group2 = matcher.group(2);
+        	str=str.replace(group, group1+" "+group2);
+    	}
+    	//str=matcher.replaceAll("");
+        
+        pattern = Pattern.compile("\\{\\{(.)+?\\}\\}");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            String group = matcher.group(0);
+            deleted.add(group);
+        }
+        str=matcher.replaceAll("");
+        
+    	pattern = Pattern.compile("<ref([^>])+?\\/>");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            String group = matcher.group(0);
+            deleted.add(group);
+        }
+        str=matcher.replaceAll("");
+        
+    
+        pattern = Pattern.compile("(<ref(.|\n)*?\\/ref>)+?|\\[\\[Catégorie:((.)+?)\\]\\]|\\[\\[Fichier:((.)+?)\\]\\]\n|\\[http((.)+?)\\]|<!--(.)?-->");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            String group = matcher.group(0);
+            deleted.add(group);
+        }
+        str=matcher.replaceAll("");
+        
+        pattern = Pattern.compile("\\[\\[(Image:(.)+?)\\]\\]\n");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+        	String group = matcher.group(0);
+        	deleted.add(group);
+    	}
+        str=matcher.replaceAll("");
+    
+        pattern = Pattern.compile("\\[\\[([^|]+?)\\]\\]");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+        	String group = matcher.group(0);
+        	String group1 = matcher.group(1);
+        	str=str.replace(group, group1);
+            	linksWiki.add(group1);            
+    	}
+    
+    	pattern = Pattern.compile("\\[\\[((.)+?\\|((.)+?))\\]\\]");
+    	matcher = pattern.matcher(str);
+    	while (matcher.find()) {
+        	String group = matcher.group(0);
+    		String group1 = matcher.group(3);
+        	str=str.replace(group, group1);
+        	group=group.replace(group1, "");
+                deleted.add(group);
+        	linksWiki.add(group1);
+    	}
+    
+    
+    	str=str.replaceAll("<br(\\s)*?/>","\n");
+    
+        pattern = Pattern.compile("[a-zA-Z\\s]''((.)+?)''\\s");
+        matcher = pattern.matcher(str);
+    	while (matcher.find()) {
+            	String group = matcher.group(1);
+            if (group.charAt(group.length()-1)!='\'') {
+            	if (group.length()>1) 
+            		italic.add(group);
+            	}
+            else if (group.charAt(group.length()-2)!='\'') {
+            	group = group.substring(1, group.length()-1); 
+            	if (group.length()>1) 
+            		bold.add(group);
+            	}
+            else {
+            	group = group.substring(3,group.length()-3); 
+            	if (group.length()>1) boldItalic.add(group);
+            	}
+        }
+    	str=str.replaceAll("l(')+","l'");
+    	str=str.replaceAll("L(')+","L'");
+        str=str.replaceAll("('){2,}","");
+    
+        pattern = Pattern.compile("==((.)+)==");
+        matcher = pattern.matcher(str);
+        while (matcher.find()) {
+        	String group = matcher.group(1).trim();
+            sectionTitles.add(group);
+        }
+    
+    	pattern = Pattern.compile("((.|\n)+?)\n==");
+    	matcher = pattern.matcher(str);
+    	while (matcher.find()) {
+    		String group = matcher.group(1).trim();
+	        sectionTexts.add(group);
+        }
+    	
+    	pattern = Pattern.compile("<!\\-\\-\n(.)+?\n\\-\\->");
+    	matcher = pattern.matcher(str);
+    	while (matcher.find()) {
+    		String group = matcher.group(1);
+	        deleted.add(group);
+        }
+    	str=matcher.replaceAll("");
+    	
+    	str=str.replaceAll("\\(|\\)","");
+    	//str=str.replaceAll("\\s\\((.)+?\\)","");
+    
+    	str=str.replaceAll("(\\h){2,}"," ");
+    	str=str.replaceAll("(\n){3,}","\n\n");
+    	str=str.replace("."," .");
+    	str=str.replace(","," ,");
+    	str=str.replace("’","'");
+    	str=str.replace("(,)+",",");
+    	str=str.trim();
+		return str;
+		
 	}
+
 	
 	
 
 	
-	/*public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		
-		File file = new File("/home/smehdi254/Desktop/Kraya/TER/Articles/R/Radiographie du thorax.txt");
+		Parser c = new Parser();
+		
+		File file = new File("Text.txt");
 		FileInputStream fis = new FileInputStream(file);
 		byte[] data = new byte[(int) file.length()];
 		fis.read(data);
 		fis.close();
-		String str = new String(, "UTF-8");
+		String str = new String(data, "UTF-8");
 		
-		Parser c = new Parser();
-		
-		Pattern pattern = Pattern.compile("\\{\\{Lang\\|\\w\\w\\|((.)+?)\\}\\}");
-        	Matcher matcher = pattern.matcher(str);
-        	while (matcher.find()) {
-	        	String group = matcher.group(0);
-	        	String group1 = matcher.group(1);
-	        	str=str.replace(group, group1);
-	        	group=group.replace(group1, "");
-	            c.deleted.add(group);
-	        }
-        
-	        pattern = Pattern.compile("\\{\\{\\w\\w\\}\\}");
-	        matcher = pattern.matcher(str);
-	        while (matcher.find()) {
-	            String group = matcher.group(0);
-	            c.deleted.add(group);
-        	}
-        	str=matcher.replaceAll("");
-	        
-	        pattern = Pattern.compile("\\{\\{(.)+?\\}\\}");
-	        matcher = pattern.matcher(str);
-	        while (matcher.find()) {
-	            String group = matcher.group(0);
-	            c.deleted.add(group);
-	        }
-	        str=matcher.replaceAll("");
-	        
-        	pattern = Pattern.compile("<ref([^>])+?\\/>");
-	        matcher = pattern.matcher(str);
-	        while (matcher.find()) {
-	            String group = matcher.group(0);
-	            c.deleted.add(group);
-	        }
-	        str=matcher.replaceAll("");
-	        
-        
-	        pattern = Pattern.compile("(<ref(.|\n)*?\\/ref>)+?|\\[\\[CatÃ©gorie:((.)+?)\\]\\]|\\[\\[Fichier:((.)+?)\\]\\]\n|\\[http((.)+?)\\]|<!--(.)?-->");
-        	matcher = pattern.matcher(str);
-	        while (matcher.find()) {
-	            String group = matcher.group(0);
-	            c.deleted.add(group);
-	        }
-	        str=matcher.replaceAll("");
-        
-	        pattern = Pattern.compile("\\[\\[([^|]+?)\\]\\]");
-	        matcher = pattern.matcher(str);
-	        while (matcher.find()) {
-	        	String group = matcher.group(0);
-	        	String group1 = matcher.group(1);
-	        	str=str.replace(group, group1);
-	            	c.linksWiki.add(group1);            
-        	}
-        
-        	pattern = Pattern.compile("\\[\\[((.)+?\\|((.)+?))\\]\\]");
-        	matcher = pattern.matcher(str);
-        	while (matcher.find()) {
-	        	String group = matcher.group(0);
-        		String group1 = matcher.group(3);
-	        	str=str.replace(group, group1);
-	        	group=group.replace(group1, "");
-	                c.deleted.add(group);
-	        	c.linksWiki.add(group1);
-        	}
-        
-        
-        	str=str.replaceAll("<br(\\s)*?/>","\n");
-        
-	        pattern = Pattern.compile("''((.)+)''");
-	        matcher = pattern.matcher(str);
-        	while (matcher.find()) {
-	            	String group = matcher.group(1);
-	            if (group.charAt(0)!='\'') {if (group.length()>1) c.italic.add(group);}
-	            else if (group.charAt(1)!='\'') {group = group.substring(1, group.length()-1); if (group.length()>1) c.bold.add(group);}
-	            else {group = group.substring(3,group.length()-3); if (group.length()>1) c.boldItalic.add(group);}
-	        }
-	        str=str.replaceAll("('){2,}","");
-        
-	        pattern = Pattern.compile("==((.)+)==");
-	        matcher = pattern.matcher(str);
-	        while (matcher.find()) {
-	        	String group = matcher.group(1).trim();
-	            c.sectionTitles.add(group);
-	        }
-        
-        	pattern = Pattern.compile("((.|\n)+?)\n==");
-        	matcher = pattern.matcher(str);
-        	while (matcher.find()) {
-        		String group = matcher.group(1).trim();
-		        c.sectionTexts.add(group);
-	        }
-        
-        	str=str.replaceAll("(\\h){2,}"," ");
-        	str=str.replaceAll("(\n){3,}","\n\n");
-        	str=str.trim();
-		
+		System.out.println(c.cleanText(str));
+			
 	}
 
-}*/
+}
 
